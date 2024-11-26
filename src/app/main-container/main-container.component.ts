ed43dac64efae057b25e902257145ff046e97b05
maksmuehlbauer <nakeda1987@gmail.com>
2024-11-26 20:17:33 +0100

    add header

--- src/app/main-container/main-container.component.ts
+++ src/app/main-container/main-container.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-main-container',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.scss'
 })
