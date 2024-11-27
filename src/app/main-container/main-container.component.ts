0d0fb7733981f49d9a3a7368c8f16799ca3e2f9e
maksmuehlbauer <nakeda1987@gmail.com>
2024-11-27 20:37:48 +0100

    news section include

--- src/app/main-container/main-container.component.ts
+++ src/app/main-container/main-container.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { LandingComponent } from "./landing/landing.component";
+import { NewsComponent } from "./news/news.component";
 
 @Component({
   selector: 'app-main-container',
   standalone: true,
-  imports: [HeaderComponent, LandingComponent],
+  imports: [HeaderComponent, LandingComponent, NewsComponent],
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.scss'
 })
