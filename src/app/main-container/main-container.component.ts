61363121937da8401658fa1c8c2994d6e176f75e
maksmuehlbauer <nakeda1987@gmail.com>
2024-11-27 19:40:21 +0100

    section-landing

--- src/app/main-container/main-container.component.ts
+++ src/app/main-container/main-container.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
+import { LandingComponent } from "./landing/landing.component";
 
 @Component({
   selector: 'app-main-container',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, LandingComponent],
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.scss'
 })
