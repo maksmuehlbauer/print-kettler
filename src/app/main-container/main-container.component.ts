e2004a79b25a64f7e33d7170f8b2d53f44d202b8
maksmuehlbauer <nakeda1987@gmail.com>
2024-12-02 20:50:22 +0100

    iadd Q&Acomp nad footer

--- src/app/main-container/main-container.component.ts
+++ src/app/main-container/main-container.component.ts
@@ -4,11 +4,13 @@ import { LandingComponent } from "./landing/landing.component";
 import { NewsComponent } from "./news/news.component";
 import { OfferingsComponent } from "./offerings/offerings.component";
 import { ExpierienceComponent } from "./expierience/expierience.component";
+import { QAndAComponent } from "./q-and-a/q-and-a.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-main-container',
   standalone: true,
-  imports: [HeaderComponent, LandingComponent, NewsComponent, OfferingsComponent, ExpierienceComponent],
+  imports: [HeaderComponent, LandingComponent, NewsComponent, OfferingsComponent, ExpierienceComponent, QAndAComponent, FooterComponent],
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.scss'
 })
