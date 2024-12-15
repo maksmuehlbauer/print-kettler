067c5276a412b7c59096f312d30e485f22b43aba
maksmuehlbauer <nakeda1987@gmail.com>
2024-12-15 12:18:54 +0100

    add form HTML

--- src/app/main-container/contact/contact.component.ts
+++ src/app/main-container/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouteCalcComponent } from "./route-calc/route-calc.component";
+import { MsgFormComponent } from "./msg-form/msg-form.component";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [RouteCalcComponent],
+  imports: [RouteCalcComponent, MsgFormComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
---
+++ src/app/main-container/contact/msg-form/msg-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-msg-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './msg-form.component.html',
+  styleUrl: './msg-form.component.scss'
+})
+export class MsgFormComponent {
+
+}
