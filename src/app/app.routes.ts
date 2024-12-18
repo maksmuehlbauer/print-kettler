89739d4e3363d2c4a7c43ceec5537edcca6986f2
maksmuehlbauer <nakeda1987@gmail.com>
2024-12-18 21:26:13 +0100

    add service comp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MainContainerComponent } from './main-container/main-container.component';
 import { ContactComponent } from './main-container/contact/contact.component';
 import { AboutUsComponent } from './main-container/about-us/about-us.component';
+import { ServiceComponent } from './main-container/service/service.component';
 
 export const routes: Routes = [
     { path: '', component: MainContainerComponent },
     { path: 'contact', component: ContactComponent },
-    { path: 'aboutus', component: AboutUsComponent }
+    { path: 'aboutus', component: AboutUsComponent },
+    { path: 'service', component: ServiceComponent }
 ];
