c0cc307ebca55c10aab27f0ca010cb2a7f050074
maksmuehlbauer <nakeda1987@gmail.com>
2025-01-03 18:09:12 +0100

    add hover and highlighting header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { UploadFormComponent } from "../upload-form/upload-form.component";
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [UploadFormComponent, CommonModule],
+  imports: [UploadFormComponent, CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
   uplaodDialogOpen: boolean = false;
+  performanceHover = false;
 
   openUploadDialog() {
     this.uplaodDialogOpen = true;
