6550748eea953bd972d0a24656c1964447b9294e
maksmuehlbauer <nakeda1987@gmail.com>
2025-01-06 19:03:17 +0100

    add undercategory highlighting

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { UploadFormComponent } from "../upload-form/upload-form.component";
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -14,6 +14,15 @@ export class HeaderComponent {
 
   uplaodDialogOpen: boolean = false;
   performanceHover = false;
+
+  constructor(private router: Router) {
+
+  }
+
+  isPerformanceActive(): boolean {
+    const currentRoute = this.router.url;
+    return currentRoute.startsWith('/performance') || currentRoute.startsWith('/sub')
+  }
 
   openUploadDialog() {
     this.uplaodDialogOpen = true;
