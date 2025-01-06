d8e3a85165036419a02b00b0f73771ba2b1c001e
maksmuehlbauer <nakeda1987@gmail.com>
2025-01-06 19:45:16 +0100

    fixing service index anchor

--- src/app/main-container/service/service.component.ts
+++ src/app/main-container/service/service.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-service',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './service.component.html',
   styleUrl: './service.component.scss'
 })
 export class ServiceComponent {
+
+  scrollTo(linkId: string): void {
+    const element = document.getElementById(linkId);
+    if (element) {
+      const yOffset = -128;
+      const yPosition = element.getBoundingClientRect().top + window.scrollY + yOffset;
+      window.scrollTo({ top: yPosition, behavior: 'smooth' });
+    }
+  }
 
 }
